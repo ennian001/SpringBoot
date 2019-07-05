fd4320775572983fc100074cdd4ac2a83487c9c3
ennian <sunennian@sina.com>
2019-07-05 11:19:14 +0800

    缓存项目 启动类 实现

--- cache/src/main/java/com/spring/cache/SpringBootCacheApplication.java
+++ cache/src/main/java/com/spring/cache/SpringBootCacheApplication.java
@@ -1,10 +1,15 @@
 package com.spring.cache;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@MapperScan("com.spring.cache.mapper")
 @SpringBootApplication
+@EnableCaching
 public class SpringBootCacheApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(SpringBootCacheApplication.class,args);
     }
 }
