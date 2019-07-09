6d0b5cab75fefd7fcb70888cd69ef63142be7c1c
sunennian <sunennian@sina.com>
2019-07-09 20:01:46 +0800

    KeyGeneration，重新添加

--- cache/src/main/java/com/spring/cache/config/MyRedisConfig.java
+++ cache/src/main/java/com/spring/cache/config/MyRedisConfig.java
@@ -3,11 +3,14 @@ package com.spring.cache.config;
 import com.spring.cache.bean.Employee;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 import java.net.UnknownHostException;
+import java.util.List;
+
 @Configuration
 public class MyRedisConfig {
     @Bean
@@ -17,5 +20,14 @@ public class MyRedisConfig {
         Jackson2JsonRedisSerializer<Employee> serializer = new Jackson2JsonRedisSerializer<Employee>(Employee.class);
         template.setDefaultSerializer(serializer);
         return template;
+    }
+    //CacheManagerCustomizers 在RedisCacheConfiguration中用来定义定制一些缓存的默认规则
+    //@ConditionalOnMissingBean({CacheManager.class})  如果没有这个类
+    @Bean
+    public RedisCacheManager cacheManager(RedisTemplate<Object,Employee> employeeRedisTemplate) {
+        RedisCacheManager cacheManager = new RedisCacheManager(employeeRedisTemplate);
+        //使用前缀，默认会将cacheName做为前缀(emp:1)
+        cacheManager.setUsePrefix(true);
+        return cacheManager;
     }
 }
