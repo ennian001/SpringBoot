585fb546420c548862e69c8dcadb2883639ce8bb
sunennian <sunennian@sina.com>
2019-07-29 17:52:40 +0800

    增加分布式锁，AOP实现

--- postgrelsql/src/main/java/study/com/cn/postgrelsql/MainApplication.java
+++ postgrelsql/src/main/java/study/com/cn/postgrelsql/MainApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan(value = "study.com.cn.postgrelsql.mapper")
 @SpringBootApplication
 public class MainApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class,args);
     }
